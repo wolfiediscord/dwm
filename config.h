ecca2f558b6140ebd84afbe02f75d209b1e865d5
wolfiediscord <wolfiediscord@gmail.com>
2021-01-10 17:46:50 -0600

    Add chrome icon & change browser to brave

--- config.h
+++ config.h
@@ -30,7 +30,7 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -68,7 +68,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[] = {"firefox-esr", NULL};
+static const char *browsercmd[] = {"brave-browser", NULL};
 static const char *filebrowsercmd[] = {"thunar", NULL};
 
 static Key keys[] = {
