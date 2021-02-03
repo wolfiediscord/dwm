fba5d76d3159838ae5983d49891ccba6d051c77e
wolfiediscord <wolfiediscord@gmail.com>
2021-02-02 20:10:09 -0600

    Change theme to gruvbox

--- config.h
+++ config.h
@@ -6,17 +6,19 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 25;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height. REMEMBER TO CHANGE DMENU IF YOU CHANGE THIS */
 static const char *fonts[]          = { "TerminessTTF Nerd Font:size=12" };
 static const char dmenufont[]       = "TerminessTTF Nerd Font:size=12";
-static const char col_gray1[]       = "#2E3440";
+static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#5E81AC";
+static const char col_orange[]      = "#d65d0e";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_orange,  col_orange  },
 };
 
 static const char *const autostart[] = {
@@ -66,10 +68,11 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_orange, "-sf", col_gray4, "-h", "25", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[] = {"brave-browser", NULL};
 static const char *filebrowsercmd[] = {"thunar", NULL};
+static const char *screenshotcmd[] = {"flameshot", "gui", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -77,6 +80,7 @@ static Key keys[] = {
 	{ MODKEY,           		XK_x,	   spawn,          {.v = termcmd } },
 	{ MODKEY,			XK_b,	   spawn,	   {.v = browsercmd } },
 	{ MODKEY,			XK_e,      spawn, 	   {.v = filebrowsercmd } },
+	{ MODKEY|ShiftMask,		XK_p,      spawn, 	   {.v = screenshotcmd } },
 	{ MODKEY,			XK_s,      spawn, 	   SHCMD("slock") },
 	{ MODKEY|ShiftMask,		XK_s,	   spawn,	   SHCMD("systemctl suspend") },
 	{ MODKEY,			XK_minus,  spawn, 	   SHCMD("amixer -D pulse sset Master 5%-") },
